a610580232b2612ac6d67508c306b3da13d3a791
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-01-29 19:14:45 +0100

    add: show type

---
+++ resources/views/admin/types/show.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <h2>{{ $type->name }}</h2>
+
+        <hr>
+        @if (count($type->projects) > 0)
+            <h3>Projects with type: {{ $type->name }}</h3>
+            <ul>
+                @foreach ($type->projects as $project)
+                    <li>
+                        <a href="{{ route('admin.projects.show', ['project' => $project->slug]) }}">{{ $project->title }}</a>
+                    </li>
+                @endforeach
+            </ul>
+        @else
+            <p>No project with this type!</p>            
+        @endif
+       
+    </div>
+@endsection
